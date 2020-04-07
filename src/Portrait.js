f8f38b64dda9d0603bdd3b543f150d75e017420b
unknown <yeehawbw@gmail.com>
2020-04-07 12:08:41 -0400

    Implement portrait images

--- src/Portrait.js
+++ src/Portrait.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function Portrait() {
-  return <div className='Portrait'></div>;
+export default function Portrait(props) {
+  console.log(props.image);
+  return (
+    <div
+      className='Portrait'
+      style={{ backgroundImage: `url(${props.image})` }}></div>
+  );
 }
