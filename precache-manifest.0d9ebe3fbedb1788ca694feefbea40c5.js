fb7df8651137b11d6034dace6edd82aa1c793f1c
unknown <yeehawbw@gmail.com>
2020-04-10 17:31:14 -0400

    Updates

---
+++ precache-manifest.0d9ebe3fbedb1788ca694feefbea40c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95a0732c086bf57802c34bd4bce0cb7",
+    "url": "/poke-card-scrambler/index.html"
+  },
+  {
+    "revision": "14d9ceeb346eb594043f",
+    "url": "/poke-card-scrambler/static/css/main.6ca7cd89.chunk.css"
+  },
+  {
+    "revision": "62e6b85f831f667de7f9",
+    "url": "/poke-card-scrambler/static/js/2.13ae3798.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poke-card-scrambler/static/js/2.13ae3798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d9ceeb346eb594043f",
+    "url": "/poke-card-scrambler/static/js/main.fa707ecf.chunk.js"
+  },
+  {
+    "revision": "5cc89f065b1ef1e0f739",
+    "url": "/poke-card-scrambler/static/js/runtime-main.85c34a08.js"
+  },
+  {
+    "revision": "b4ff4beb8ba23c99d14af0576c28fe51",
+    "url": "/poke-card-scrambler/static/media/back.b4ff4beb.jpg"
+  },
+  {
+    "revision": "8bb203927b273721d66d77ef194cea09",
+    "url": "/poke-card-scrambler/static/media/logo.8bb20392.svg"
+  }
+]);
